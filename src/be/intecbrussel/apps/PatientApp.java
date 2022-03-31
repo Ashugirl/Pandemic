c8412feeba57a60236182b680fcd103170a57fad
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-03-31 20:06:12 +0200

    Initial commit

---
+++ src/be/intecbrussel/apps/PatientApp.java
@@ -0,0 +1,18 @@
+package be.intecbrussel.apps;
+
+import be.intecbrussel.classes.Patient;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PatientApp {
+    public static void main(String[] args) {
+
+        Set<Patient> patientSet = new LinkedHashSet<Patient>();
+
+
+
+    }
+}
